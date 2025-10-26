cac3746ee4b8c9344f99f95015ab24700cc9261c
Vincent <vincenttueminh@gmail.com>
2025-10-25 20:06:39 -0400

    class 0

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,72 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.components.Controller;
+import frc.robot.components.DriveFrame;
+import frc.robot.data.StickPosition;
+
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  private final Controller controller;
+  private final DriveFrame driveFrame;
+
+  public Robot() {
+    controller = new Controller();
+    driveFrame = new DriveFrame();
+  }
+
+  @Override
+  public void robotPeriodic() {}
+
+
+  @Override
+  public void autonomousInit() {}
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {}
+
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {}
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {
+  }
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+}
---
+++ src/main/java/frc/robot/components/DriveFrame.java
@@ -0,0 +1,33 @@
+package frc.robot.components;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkFlex;
+import frc.robot.data.StickPosition;
+
+public class DriveFrame {
+    private final DifferentialDrive differentialDrive;
+
+    public DriveFrame() {
+        var leftController = new PWMSparkFlex(1);
+        leftController.setInverted(false);
+
+        var rightController = new PWMSparkFlex(0);
+        rightController.setInverted(true);
+
+        differentialDrive = new DifferentialDrive(leftController, rightController);
+
+    }
+
+    public void poll() {}
+
+    
+    public void singleStickDrive(final StickPosition stickPosition) {
+        // FIXME: Use PID
+        differentialDrive.arcadeDrive(stickPosition.y(), stickPosition.x());
+    }
+
+    public void dualStickDrive(final StickPosition leftStickPosition, final StickPosition rightStickPosition) {
+        // FIXME: Use PID
+        differentialDrive.tankDrive(leftStickPosition.y(), rightStickPosition.y());
+    }
+}
