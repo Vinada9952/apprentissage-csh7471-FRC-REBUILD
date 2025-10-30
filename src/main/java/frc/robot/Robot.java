18cbcbf2717e00c5310807f37eb9714b2dddbabf
Louka <Nero_pro@outlook.com>
2025-10-30 17:32:42 -0400

    Add right_joystick to teleopPeriodic

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -44,8 +44,9 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    StickPosition left_joystick = controller.getLeftStickPosition(); // Ici, le code va chercher la position du joystick gauche de la manette et va le stocker dans une variable
     
+   StickPosition left_joystick = controller.getLeftStickPosition(); // Ici, le code va chercher la position du joystick gauche de la manette et va le stocker dans une variable
+   StickPosition right_joystick = controller.getRightStickPosition(); 
     // TODO: Créer une variable right_joystick de type StickPosition pour le joystick droit
     // TODO: Créer une variable both_sticks de type Stickposition qui contient la donnée x tu stick droit et x du stick gauche
     
