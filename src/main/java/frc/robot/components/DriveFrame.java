37465af49a7cc2c5401b535e3ef462e8f9f20faa
thekirboking <jonathan.chalifoux@outlook.com>
2025-11-05 18:09:50 -0500

    jsp

--- src/main/java/frc/robot/components/DriveFrame.java
+++ src/main/java/frc/robot/components/DriveFrame.java
@@ -27,6 +27,7 @@ public class DriveFrame {
     }
 
     public void dualStickDrive( final StickPosition leftStickPosition, final StickPosition rightStickPosition ) {
+        differentialDrive.arcadeDrive(leftStickPosition.x(), rightStickPosition.y())
         // TODO: faire la même chose qu'au dernier cours, mais dans cette fonction
         // pour envoyer une donnée au moteurs, on va utiliser la fonction arcadeDrive du differentialDrive
         // differentialDrive est une variable qui contient en données les 2 moteurs, donc on fait juste lui envoyer un joystick et elle s'occupe du reste
